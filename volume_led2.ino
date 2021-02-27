f4b8f73ff14f2ee58d74e79de2e268588e2756f3
PhysKen <62343651+phys-ken@users.noreply.github.com>
2021-02-27 21:41:03 +0900

    ledLevelの変数を追加

--- volume_led2.ino
+++ volume_led2.ino
@@ -5,7 +5,8 @@
 int sensorPin = 25;        //esp32の25pinにマイクのout マイクアンプのVccは3.3V
 int sensorValue = 0;       //初期値
 int ledbit = 3;            //3ビット8段階でLEDを発光
-int ledLevel = 2 ^ ledbit; //LEDの輝度量子化。ledbit変数に依存させる。//これがうまく動かない！！！！
+int ledLevel = pow(2, ledbit); //LEDの輝度量子化。ledbit変数に依存させる。//
+
 
 float presensorValue = 0; //フィルターの初期値
 const int ledPin = A19;   //A19は、ESP32の26ピン,LEDのPWM制御
@@ -18,6 +19,7 @@ void setup()
   // ledPinをチャネル0へ接続
   ledcAttachPin(ledPin, 0);
   Serial.begin(9600);
+  Serial.println(ledLevel);
 }
 
 void loop()
@@ -30,6 +32,7 @@ void loop()
   //入力値を平滑化
   sensorValue = 0.9 * presensorValue + 0.1 * sensorValue; // ローパスフィルター適用 0.9はなんとなく。
   presensorValue = sensorValue;                           // 前回値の保存
+
 
   sensorValue = map(sensorValue, 0, 1870, 0, ledLevel); //ledLevel 段階に丸める
   Serial.println(sensorValue);                   //シリアルモニタに表示、シリアルプロッタで見るのがおすすめ
